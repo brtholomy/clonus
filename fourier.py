737106544bba64044b9af9d529024c8cfd0631dc
bth <gh@hautogdoad.com>
2024-07-26 13:31:11 -0700

    add click CLI and CompareToPink
    still not quite the right graph, should probably be another spectrogram of the diff

--- fourier.py
+++ fourier.py
@@ -1,5 +1,6 @@
 import visualization as viz
 
+import click
 import audioflux as af
 import numpy as np
 import matplotlib.pyplot as plt
@@ -10,9 +11,14 @@ from audioflux.utils import power_to_db
 from audioflux.display import fill_spec
 
 
+PINKPATH = './samps/pink.wav'
+
+
 def GetBFT(path):
     audio_arr, sr = af.read(path)
-    # Create BFT object of Linser(STFT)
+    # num : num of frequency bins, meaning a FFT is performed for that f envelope
+    # radix2_exp : fft_length=2**radix2_exp : default 2^12 = 4096
+    # slide_length : data sampling size, by default = fft_length / 4
     obj = af.BFT(num=2049, radix2_exp=12, samplate=sr, low_fre=0., high_fre=20_000.,
                  window_type=WindowType.HANN, slide_length=1024,
                  scale_type=SpectralFilterBankScaleType.LINEAR,
@@ -44,10 +50,36 @@ def ShortTimeFourierTransform(path):
     spec_dB_arr = GetDbSpectrogram(obj, audio_arr)
     return obj, audio_arr, spec_dB_arr
 
-def main():
-    path = './samps/pink.wav'
+def CompareToPink(path):
+    pink_obj, pink_audio_arr, pink_spec_dB_arr = ShortTimeFourierTransform(PINKPATH)
     obj, audio_arr, spec_dB_arr = ShortTimeFourierTransform(path)
+    # change length so that we can simply diff the two
+    spec_dB_arr_fitted = np.resize(spec_dB_arr, pink_spec_dB_arr.shape)
+    diff = pink_spec_dB_arr - spec_dB_arr_fitted
+    print(f'{diff.shape = }')
+    # TODO: this isn't the right graph:
+    plt.plot(diff)
+    viz.PlotShow()
+
+@click.command()
+@click.option(
+    '--file_path', '-f',
+    default='./samps/pink.wav',
+    type=str,
+    show_default=True,
+    help='wav file to analyze'
+)
+@click.option(
+    '--compare_to_pink', '-c',
+    is_flag=True,
+    default=False,
+    help='generate a diff plot against pink noise reference'
+)
+def main(file_path, compare_to_pink):
+    obj, audio_arr, spec_dB_arr = ShortTimeFourierTransform(file_path)
     PlotSpectrogram(obj, audio_arr, spec_dB_arr)
+    if compare_to_pink:
+        CompareToPink(file_path)
 
 if __name__ == "__main__":
     main()
