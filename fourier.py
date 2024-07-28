e49684add148385f0a5f7796e7d4bc0fba25e45b
bth <gh@hautogdoad.com>
2024-07-27 18:58:38 -0700

    handle sizes smaller than the pink reference, dynamically resizing the FFT window

--- fourier.py
+++ fourier.py
@@ -4,6 +4,7 @@ import click
 import audioflux as af
 import numpy as np
 import matplotlib.pyplot as plt
+import math
 
 from audioflux.type import (SpectralFilterBankScaleType, SpectralFilterBankStyleType,
                             WindowType, SpectralDataType)
@@ -11,12 +12,26 @@ from audioflux.utils import power_to_db
 from audioflux.display import fill_spec
 
 
+def CalculateFFTLengthExp(audio_arr):
+    # this means 2**12 if the sample is big enough, otherwise closest power of 2
+    # based on the sample length:
+    radix_length = math.log(len(audio_arr), 2)
+    return 12 if radix_length > 12 else math.floor(radix_length)
+
+def CalculateNumberFrequencyBins(two_exp):
+    # NOTE: this is just a dynamic sizing based on their example of 2049
+    # matching 2*12:
+    return (2**(two_exp - 1)) + 1
+
 def GetBFT(path):
     audio_arr, sr = af.read(path)
+
     # num : num of frequency bins, meaning a FFT is performed for that f envelope
     # radix2_exp : fft_length=2**radix2_exp : default 2^12 = 4096
     # slide_length : data sampling size, by default = fft_length / 4
-    obj = af.BFT(num=2049, radix2_exp=12, samplate=sr, low_fre=0., high_fre=20_000.,
+    two_exp = CalculateFFTLengthExp(audio_arr)
+    num_f = CalculateNumberFrequencyBins(two_exp)
+    obj = af.BFT(num=num_f, radix2_exp=two_exp, samplate=sr, low_fre=0., high_fre=20_000.,
                  window_type=WindowType.HANN, slide_length=1024,
                  scale_type=SpectralFilterBankScaleType.LINEAR,
                  style_type=SpectralFilterBankStyleType.SLANEY,
@@ -51,11 +66,16 @@ def Compare(path, compare_path):
     ref_obj, ref_audio_arr, ref_spec_dB_arr = ShortTimeFourierTransform(compare_path)
     obj, audio_arr, spec_dB_arr = ShortTimeFourierTransform(path)
 
-    # change length of input sample so that we can simply diff the two
-    spec_dB_arr_fitted = np.resize(spec_dB_arr, ref_spec_dB_arr.shape)
-    audio_arr_fitted = np.resize(audio_arr, ref_audio_arr.shape)
-    spec_diff = ref_spec_dB_arr - spec_dB_arr_fitted
-    audio_diff = ref_audio_arr - audio_arr_fitted
+    # change length of the longer sample so that we can simply diff the two
+    if len(ref_audio_arr) < len(audio_arr):
+        spec_dB_arr = np.resize(spec_dB_arr, ref_spec_dB_arr.shape)
+        audio_arr = np.resize(audio_arr, ref_audio_arr.shape)
+    else:
+        ref_spec_dB_arr = np.resize(ref_spec_dB_arr, spec_dB_arr.shape)
+        ref_audio_arr = np.resize(ref_audio_arr, audio_arr.shape)
+
+    spec_diff = ref_spec_dB_arr - spec_dB_arr
+    audio_diff = ref_audio_arr - audio_arr
     # can reuse this obj because it's just a specification:
     PlotSpectrogram(obj, audio_diff, spec_diff, 'diff of %s and %s' % (path, compare_path))
 
